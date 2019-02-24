ed9465b372745b4f71fdb672afd77f664b82914a
Leevi Uotinen <uotinen.leevi@hotmail.com>
2019-02-24 08:27:56 +0200

    Add action to fetch groups for user

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,12 +1,15 @@
 const SET_JWT = 'SET_JWT';
 const CLEAR_JWT = 'CLEAR_JWT';
+const SET_GROUPS = 'SET_GROUPS';
 
 const state = {
   jwt: null,
   userUuid: null,
   firstName: null,
   lastName: null,
-  email: null
+  email: null,
+  groups: {},
+  watchlists: {}
 };
 
 const getters = {
@@ -14,12 +17,14 @@ const getters = {
 };
 
 const actions = {
-  initialize({ commit }) {
+  initialize({ commit, dispatch }) {
     const jwt = sessionStorage.getItem('jwt');
-    if (jwt)
+    if (jwt) {
       commit(SET_JWT, { token: jwt });
+      dispatch('fetchGroups');
+    }
   },
-  logIn({ commit }, { email, password }) {
+  logIn({ commit, dispatch }, { email, password }) {
     return new Promise((resolve, reject) => {
       var request = new XMLHttpRequest();
       request.open('POST', 'https://api.projectfina.com/auth', true);
@@ -29,16 +34,35 @@ const actions = {
         if (this.status >= 200 && this.status < 400) {
           const data = JSON.parse(this.response);
           commit(SET_JWT, data);
+          dispatch('fetchGroups').finally(resolve());
+        } else
+          reject();
+      };
+
+      request.send(JSON.stringify({ email, password }));
+    });
+  },
+  logOut({ commit }) {
+    commit(CLEAR_JWT);
+  },
+  fetchGroups({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      var request = new XMLHttpRequest();
+      request.open('GET', 'https://api.projectfina.com/user/groups', true);
+      request.setRequestHeader('authorization', `Bearer ${state.jwt}`);
+
+      request.onerror = reject;
+      request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+          const data = JSON.parse(this.response);
+          commit(SET_GROUPS, data);
           resolve();
         } else
           reject();
       };
 
-      request.send(JSON.stringify({ email: email, password: password }));
+      request.send();
     });
-  },
-  logOut({ commit }) {
-    commit(CLEAR_JWT);
   }
 };
 
@@ -53,8 +77,17 @@ const mutations = {
     sessionStorage.setItem('jwt', token);
   },
   [CLEAR_JWT](state) {
-    Object.keys(state).forEach(k => state[k] = null);
+    state.jwt = null;
+    state.userUuid = null;
+    state.firstName = null;
+    state.lastName = null;
+    state.email = null;
+    state.groups = {};
+    state.watchlists = {};
     sessionStorage.clear();
+  },
+  [SET_GROUPS](state, { groups }) {
+    state.groups = groups;
   }
 };
 
