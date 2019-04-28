ee5dcee420fac3f27ff09ef74322e1a1ae01ef30
Leevi Uotinen <uotinen.leevi@hotmail.com>
2019-04-28 20:39:19 +0300

    Add backdrop logo to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template>
-<div></div>
+<div class="bg"></div>
 </template>
 
 <script>
-
   export default {
     components: {
     }
   }
 </script>
+
+<style scoped>
+.bg {
+  height: 100%;
+  background-size: auto 40vh;
+  background-position: center;
+  background-image: url(/assets/projectfina-logo-white.svg);
+  filter: drop-shadow(8px 12px 12px rgba(0,0,0,0.05));
+}
+</style>
